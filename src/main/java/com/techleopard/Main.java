d287f1eb2a77b2a5a78edbf1f022245d83c06997
techLeopard <adilet.satylganov@gmail.com>
2024-11-20 21:23:22 +0600

    new text message

--- src/main/java/com/techleopard/Main.java
+++ src/main/java/com/techleopard/Main.java
@@ -2,7 +2,7 @@ package com.techleopard;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("hello everybody!");
     }
 
 }
